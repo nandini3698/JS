9a905b0b63c551594426bcb6313e0bff319bab54
Nandini Roy <49457087+nandini3698@users.noreply.github.com>
2024-06-30 19:56:07 +0000

    changes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict" // Instructs engine to treat all code as newer version of JS. Currently by default all code is treated as newer version
+
+// alert( 3+3) /* will give error because we are using node js and not browser */
+
+
